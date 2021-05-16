d40e9bd47b019a313bc1b10ad8e260dfd7dd0d5e
lliksis <linsebaer@gmx.de>
2021-05-16 11:11:08 +0200

    filter vendors by bounty category

--- src/api/destiny2/vendor/vendor.ts
+++ src/api/destiny2/vendor/vendor.ts
@@ -57,61 +57,68 @@ const resolveVendors = async (
 
     for (const vendorHash in vendors.data) {
         const vendor = vendors.data[vendorHash];
-        const vendorDisplayProperties =
-            vendorDefinition[vendor.vendorHash].displayProperties;
+        if (
+            vendorDefinition[vendor.vendorHash].displayCategories.some(
+                (v) => v.identifier === "category_bounties"
+            )
+        ) {
+            const vendorDisplayProperties =
+                vendorDefinition[vendor.vendorHash].displayProperties;
 
-        // progression
-        let vendorProgression: IVendorProgression | null = null;
-        if (vendor.progression) {
-            const progressionHash = vendor.progression.progressionHash;
-            const progression = progressionDefinition[progressionHash];
-            vendorProgression = {
-                ...vendor.progression,
-                name: progression.displayProperties.name,
-                description: progression.displayProperties.description,
-                icon: bngBaseUrl + progression.displayProperties.icon,
-                unitName: progression.displayProperties.displayUnitsName,
-            };
-        }
-
-        // group
-        const groupHash = vendorGroups.data.groups.find((g) =>
-            g.vendorHashes.find((hash) => hash.toString() === vendorHash)
-        )?.vendorGroupHash;
-        const vendorGroup = vendorGroupDefintion[groupHash]?.categoryName;
-
-        // items
-        const resolvedItems: IVendorBounty[] = [];
-        const items = sales.data[vendorHash].saleItems;
-        for (const saleHash in items) {
-            const saleItemHash = items[+saleHash].itemHash;
-            const item = inventoriyItemDefinition[saleItemHash];
-            if (item.itemType === DestinyItemType.Bounty) {
-                const itemObjective =
-                    itemComponents[vendorHash].objectives.data[saleHash]
-                        .objectives[0];
-                const objective =
-                    objectiveDefinition[itemObjective.objectiveHash];
-                resolvedItems.push({
-                    ...item,
-                    displayProperties: {
-                        ...item.displayProperties,
-                        icon: bngBaseUrl + item.displayProperties.icon,
-                    },
-                    completionValue: itemObjective.completionValue,
-                    objectiveProgressDescription: objective.progressDescription,
-                });
+            // progression
+            let vendorProgression: IVendorProgression | null = null;
+            if (vendor.progression) {
+                const progressionHash = vendor.progression.progressionHash;
+                const progression = progressionDefinition[progressionHash];
+                vendorProgression = {
+                    ...vendor.progression,
+                    name: progression.displayProperties.name,
+                    description: progression.displayProperties.description,
+                    icon: bngBaseUrl + progression.displayProperties.icon,
+                    unitName: progression.displayProperties.displayUnitsName,
+                };
             }
-        }
 
-        resolvedVendors.push({
-            ...vendor,
-            ...vendorDisplayProperties,
-            icon: bngBaseUrl + vendorDisplayProperties.icon,
-            bounties: resolvedItems,
-            group: vendorGroup,
-            progression: vendorProgression,
-        });
+            // group
+            const groupHash = vendorGroups.data.groups.find((g) =>
+                g.vendorHashes.find((hash) => hash.toString() === vendorHash)
+            )?.vendorGroupHash;
+            const vendorGroup = vendorGroupDefintion[groupHash]?.categoryName;
+
+            // items
+            const resolvedItems: IVendorBounty[] = [];
+            const items = sales.data[vendorHash].saleItems;
+            for (const saleHash in items) {
+                const saleItemHash = items[+saleHash].itemHash;
+                const item = inventoriyItemDefinition[saleItemHash];
+                if (item.itemType === DestinyItemType.Bounty) {
+                    const itemObjective =
+                        itemComponents[vendorHash].objectives.data[saleHash]
+                            .objectives[0];
+                    const objective =
+                        objectiveDefinition[itemObjective.objectiveHash];
+                    resolvedItems.push({
+                        ...item,
+                        displayProperties: {
+                            ...item.displayProperties,
+                            icon: bngBaseUrl + item.displayProperties.icon,
+                        },
+                        completionValue: itemObjective.completionValue,
+                        objectiveProgressDescription:
+                            objective.progressDescription,
+                    });
+                }
+            }
+
+            resolvedVendors.push({
+                ...vendor,
+                ...vendorDisplayProperties,
+                icon: bngBaseUrl + vendorDisplayProperties.icon,
+                bounties: resolvedItems,
+                group: vendorGroup,
+                progression: vendorProgression,
+            });
+        }
     }
     return resolvedVendors;
 };
