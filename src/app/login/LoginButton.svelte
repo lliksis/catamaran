50a0bbaa5ae9bff211f7b81cd4dd9f19b90df20e
Tobias Mueller <T.Mueller@schueler.klara-oppenheimer-schule.de>
2021-05-27 20:26:04 +0200

    Refactored login button

--- src/app/login/LoginButton.svelte
+++ src/app/login/LoginButton.svelte
@@ -1,7 +1,42 @@
+<style>
+    :root {
+        --hover-border-color: #00000050;
+        --text-border-color: #00000030;
+    }
+
+    .login {
+        background-color: rgba(255, 196, 0, 0.836);
+        width: 200px;
+        height: 40px;
+        border-radius: 5px;        
+    }
+    .login:hover {
+        box-shadow: 0px 0px 0px 1px var(--hover-border-color);
+        cursor: pointer;
+    }
+
+    .login-link {
+        display: block;
+        text-align: center;
+        margin: auto;
+        margin-top: auto;
+        margin-bottom: auto;
+
+        font-weight: 600;
+        font-size: 15px;
+        color: white;
+        text-shadow: -0.5px 0 var(--text-border-color),
+            0 0.5px var(--text-border-color), 0.5px 0 var(--text-border-color),
+            0 -0.5px var(--text-border-color);
+    }
+
+</style>
+
 <script>
     export let href;
+
 </script>
 
-<a {href}>
-    <button> Login </button>
-</a>
+<div class="login">
+    <a class="login-link" {href}> Login via Bungie </a>
+</div>
---
+++ src/stories/LoginButton.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Button from "../app/login/LoginButton.svelte";
+
+</script>
+
+<Meta
+    title="Components/LoginButton"
+    component={Button}
+    argType={{
+        href: {controle: "text"}
+    }}
+/>
+
+<Template let:args>
+    <Button href={args.href} />
+</Template>
+
+<Story name="Login Button" />
