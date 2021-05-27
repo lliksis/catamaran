8c67ff31426d9465009a884f0d3d916f1e1fd83a
Tobias Mueller <T.Mueller@schueler.klara-oppenheimer-schule.de>
2021-05-27 20:26:17 +0200

    Added emblem component

---
+++ src/app/character/Emblem.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import type { IDestinyCharacterComponentOverride } from "api/destiny2/profile";
+
+    export let character: IDestinyCharacterComponentOverride;
+    export let onclick: (character: IDestinyCharacterComponentOverride) => void;
+    export let variant: "primary" | "secondary" = "primary";
+
+    const aspectRatio = variant === "primary" ? "79 / 16" : "1 / 1";
+    const imgAlt = `character emblem (${character.class}, ${character.characterId})`;
+
+</script>
+
+<div class="emblem-wrapper" on:click={() => onclick(character)} style={`background-image: url(${character.emblemBackgroundPath}); aspect-ratio: ${aspectRatio}`} id={character.characterId}>
+    <span class="emblem-light">
+        {character.light}
+    </span>
+    <div class="emblem-character-information">
+        <div class="emblem-character-class">
+            {character.class}
+        </div>
+        <div class="emblem-character-race">
+            Human
+        </div>
+    </div>
+    
+</div>
+
+<style>
+    .emblem-wrapper {
+        width: 300px;
+        background-size: auto 100%;
+    }
+
+    .emblem-character-information {
+        border: 1px solid black;
+        position: relative;
+        text-align: center;
+    }
+    .emblem-character-class{
+        color: white;
+        font-size: 20px;
+        font-weight: 600;
+    }
+    .emblem-character-race{
+        font-size: 17px;
+        color: #eee;
+    }
+    .emblem-light {
+        position: relative;
+        float: right;
+        top: 50%;
+        transform: translateY(-50%);
+        margin-right: 5px;
+        color: #ffff00;
+        font-size: 30px;
+        font-weight: 600;
+    }
+</style>
---
+++ src/stories/Emblem.stories.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import CharacterEmblem from "../app/character/Emblem.svelte";
+
+</script>
+
+<Meta
+    title="Components/Emblem"
+    component={CharacterEmblem}
+    argType={{
+        character: {
+            characterId: { control: "number" },
+            class: {
+                control: {
+                    type: "select",
+                    options: ["Hunter", "Warlock", "Titan"],
+                },
+            },
+            light: { control: "number" },
+            emblemPath: { control: "text" },
+            emblemBackgroundPath: { control: "text" },
+        },
+        variant: {
+            control: {
+                type: "inline-radio",
+                options: ["primary", "secondary"],
+            },
+        },
+    }}
+/>
+
+<Template let:args>
+    <CharacterEmblem
+        character={{
+            ...args.character,
+        }}
+        variant={args.variant}
+    />
+</Template>
+
+<Story
+    name="Primary"
+    args={{
+        character: {
+            characterId: 1234,
+            light: 1320,
+            class: "Hunter",
+            emblemBackgroundPath:
+                "https://www.bungie.net/common/destiny2_content/icons/c9045e341668eb434b68cd8d02e3ab21.jpg",
+        },
+        variant: "primary",
+    }}
+/>
+
+<Story
+    name="Secondary"
+    args={{
+        character: {
+            characterId: 1234,
+            light: 1320,
+            class: "Hunter",
+            emblemPath:
+                "https://www.bungie.net/common/destiny2_content/icons/4277426f6ffdb0cf85075c7251268374.jpg",
+        },
+        variant: "secondary",
+    }}
+/>
